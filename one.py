bc694ae5d2da4bc592a974304508764588c880c5
anfmorenoso <anfmorenoso@unal.edu.co>
2018-02-09 10:48:54 -0500

    initial commit

---
+++ one.py
@@ -0,0 +1,51 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+# import sys
+# import codecs
+# sys.stdout = codecs.getwriter("iso-8859-1")(sys.stdout, 'xmlcharrefreplace')
+
+filename = "laconcha.txt"
+f = open(filename, "w", encoding="utf8")
+g = open("wea.txt", "w", encoding="utf8")
+
+myurl = 'http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000639508'
+#myurl = 'http://scienti.colciencias.gov.co:8081/cvlac/visualizador/generarCurriculoCv.do?cod_rh=0000499625' #restrepo
+
+#opening connection
+uClient = uReq(myurl)
+page_html = uClient.read()
+uClient.close()
+
+#html parser
+# page_soup = soup(page_html, "html.parser")
+page_soup = soup(open("cvlacisp.html", encoding='latin-1'), "html.parser")
+bqs = page_soup.findAll("blockquote")
+tabs = page_soup.findAll("table")
+
+cnt = 0
+a = []
+
+#g.write( str(tabs[37]) )
+
+for n in range(1,len(tabs)):
+    #f.write( str(n) + " " + str(tabs[n]) + "\n" )
+    try:
+        tex = tabs[n].tr.td.h3.text
+        a.append(n);
+        f.write( tex + "\n" )
+    except AttributeError:
+        f.write( "lmfao " + str(n) + "\n")
+
+#imprimir arreglo de cosas que tienen título h3
+f.write( "[" )
+for i in a:
+    f.write( str(i) + " " )
+f.write( "]" )
+
+blq37 = tabs[37].findAll("blockquote")
+# cowsep = [x.strip() for x in blq37[0].text.strip().replace('\n','').split(',')]
+# for item in cowsep:
+#     if '"' in item:
+#         print(item)
+g.write( str( blq37[4].text ) )
+#g.write( str(tabs[10]) )
